0d0dd1ef18f3183fe939b4fb4856e2e9d57db2aa
foxx1337 <foxx1337@yahoo.com>
2019-12-09 19:00:09 +0100

    Use oauth2 for Google and modernize requirements

--- googleplay.py
+++ googleplay.py
@@ -1,33 +1,63 @@
 import datetime
 
 from gmusicapi import Mobileclient
+from os.path import isfile
 
 
 class GmusicApi:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
+    OAUTH_FILE = "gpm.json"
+
+    def __init__(self):
         self.api = Mobileclient()
-        result = self.api.login(self.email,
-                                self.password,
-                                Mobileclient.FROM_MAC_ADDRESS)
-        print('login for ' + self.email + ': ' + str(result))
+        if not isfile(GmusicApi.OAUTH_FILE):
+            self.oauth = self.api.perform_oauth(GmusicApi.OAUTH_FILE)
+
+        could_login = self.api.oauth_login(Mobileclient.FROM_MAC_ADDRESS, GmusicApi.OAUTH_FILE)
 
     def search(self, query, max_results=50):
         return self.api.search(query, max_results)
 
     def make_playlist(self, artist, songs):
         song_ids = []
+        show_id = 1
+        playlist_id = 1
+
         for song in songs:
             search_result = self.search(artist + ' ' + song)
             if len(search_result['song_hits']) > 0:
-                for song_hit in search_result['song_hits']:
-                    if song_hit['track']['artist'].lower() == artist.lower():
-                        song_ids.append(song_hit['track']['storeId'])
-                        break
+                song_id, album, title = self.first_album_song(search_result['song_hits'], artist)
+                if song_id:
+                    song_ids.append(song_id)
+                    print('{:02}/{:02}{:>32} - {}: {}'.format(playlist_id, show_id, song, album, title))
+                    playlist_id += 1
+                else:
+                    print('{:02}   {:>32} - ?'.format(show_id, song))
+            show_id += 1
+
         playlist_id = self.api.create_playlist(artist + '-'
-                                               + str(datetime.datetime.date(datetime.datetime.now())))
+            + str(datetime.datetime.date(datetime.datetime.now())))
         self.api.add_songs_to_playlist(playlist_id, song_ids)
+
+    def first_album_song(self, song_hits, artist):
+        candidate = (None, None, None)
+        for song_hit in song_hits:
+            if song_hit['track']['artist'].lower() == artist.lower():
+                if not self.is_low_quality(song_hit['track']['title']):
+                    return (song_hit['track']['storeId'],
+                        song_hit['track']['album'],
+                        song_hit['track']['title'])
+                elif not candidate:
+                    candidate = (song_hit['track']['storeId'],
+                        song_hit['track']['album'],
+                        song_hit['track']['title'])
+
+        return candidate
+
+    def is_low_quality(self, song):
+        title = song.lower()
+        return (('(live' in title)
+            or ('edit)' in title)
+            or ('remix' in title))
 
     def close(self):
         self.api.logout()
--- setlist.py
+++ setlist.py
@@ -9,22 +9,20 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description='Fetch artist songs from setlist.fm and create Google Play Music playlist',
         epilog='''
-User credentials for Google Play Music, together (optional now) with setlist.fm API key
-are read from a provided credentials.txt file in this format:
-    username@gmail.com:password
+User credentials for Google Play Music are stored in gpm.json and retrieved via browser here.
+Optional now setlist.fm API key is read from a provided credentials.txt file in this format:
     last.fm api key\n\n
-For the Google credentials either supply the password or a 2-factor API key.
 
 Creates a playlist called "<artist>-today's date".'''
     )
-    parser.add_argument('artist', type=str, help='the artist to search for; give exact name')
+    parser.add_argument('artist', type=str, help='the artist to search for; give exact name, don\'t fear quotes')
     arguments = parser.parse_args()
 
-    email, password, setlistFm = read_credentials()
+    setlistFm = read_credentials()
     artist = arguments.artist
     songs = get_last_songs(artist, setlistFm)
 
-    api = GmusicApi(email, password)
+    api = GmusicApi()
     api.make_playlist(artist, songs)
     api.close()
 
@@ -32,8 +30,7 @@ Creates a playlist called "<artist>-today's date".'''
 def read_credentials():
     with open('credentials.txt', 'r') as f:
         credentials = f.read()
-    google, sfm = credentials.strip().split('\n')
-    return tuple(google.strip().split(':')) + (sfm, )
+    return credentials.strip().split('\n')[0]
 
 
 if __name__ == '__main__':
